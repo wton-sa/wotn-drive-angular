5df65c7c31afc8011ca4859c546f5d9669f6bcb3
EhabZakWton <e.zakaria@wotn.com.sa>
2025-04-29 22:31:48 +0300

    phase1

---
+++ chunk-CS23HGCL.js
@@ -0,0 +1,1 @@
+import{E as o}from"./chunk-GN5XUQ4H.js";var r=class t{loginEmail;loginPassword;static \u0275fac=function(i){return new(i||t)};static \u0275prov=o({token:t,factory:t.\u0275fac,providedIn:"root"})};export{r as a};

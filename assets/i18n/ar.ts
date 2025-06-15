6e137a620d0d7ac09516844774f81d02a0469a84
EhabZak <e.zakaria@wotn.com.sa>
2025-06-15 12:11:29 +0300

    add20

---
+++ assets/i18n/ar.ts
@@ -0,0 +1,38 @@
+
+export const locale: any = {
+  lang: 'ar',
+  data: {
+    dir: "rtl",
+    RequieredField: "حقل مطلوب",
+    Save: "حفظ",
+    Upload: "رفع",
+
+    Projects: {
+      ProjectsPage: "المشروعات",
+      CreateProject: "إضافة مشروع",
+      ProjectName: "اسم المشروع",
+      WriteProjectNameHere: "اكتب اسم المشروع هنا",
+      ProjectType: "نوع المشروع",
+      SelectProjectType: "اختر نوع المشروع",
+      Event: "فعالية",
+      Conference: "مؤتمر",
+      Description: "الوصف",
+      WriteDescriptionHere: "اكتب الوصف هنا",
+      StartDate: "تاريخ البداية",
+      EndDate: "تاريخ الانتهاء",
+      Location: "الموقع",
+      WriteLocationHere: "اكتب الموقع هنا",
+      PeopleWithAccess: "صلاحيات المستخدمين",
+      EnterNameOrEmail: "ادخل الاسم أو البريد الإلكتروني",
+      AddPropjectPhoto: "أضف صورة المشروع",
+      DropImageHereOrBrowse: "قم بإسقاط الصورة هنا، أو انقر لتصفح",
+      PhotoInfo: "يفضل أن تكون الأبعاد 1600 × 1200 (4:3). يتم قبول ملفات PNG و JPG و GIF",
+      ImagePreview: "عرض الصورة",
+      EnterValidData: "قم بإدخال بيانات صحيحة",
+      ProjectAddedSuccessfully: "تم إضافة المشروع بنجاح",
+      ProjectCreationFailed: "خطأ أثناء إضافة المشروع",
+      UploadedImage: "الصورة المرفوعة",
+      EndDateMustBeGreater: "تاريخ الانتهاء يجب أن يكون أكبر من تاريخ البداية"
+    }
+  }
+};

5c5b1bf4da03290976ef7b0276b8d523eea364f4
EhabZak <e.zakaria@wotn.com.sa>
2025-07-24 10:53:42 +0300

    add 23

---
+++ chunk-WW3I3R4A.js
@@ -0,0 +1,1 @@
+import{P as t}from"./chunk-4TEO6QN3.js";var o=class r{loginEmail;loginPassword;static \u0275fac=function(s){return new(s||r)};static \u0275prov=t({token:r,factory:r.\u0275fac,providedIn:"root"})},i=class{OldPassword;Password;ConfirmPassword};export{o as a,i as b};
